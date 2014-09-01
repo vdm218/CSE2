f8970f900153acb0e36e87caef6d2cd2f89759fc
vdm218 <vdm218@lehigh.edu>
2014-09-01 00:55:00 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,34 @@
+//////////////////////////////////////////////////////////////////
+//Veronica McKinny
+//hw01
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass
+
+//  define a class
+public class WelcomeClass{
+
+//  define a main method
+  public static void main(String[] args) {
+
+//  print out the welcome message in box
+        System.out.println("    -------------   ");
+        System.out.println("    |  WELCOME  |   ");
+        System.out.println("    -------------   ");
+//  print out user id in several diamond boxes
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-V--D--M--2--1--8-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  V  V  V  V  V  V  ");
+//  print out twitter-length biography
+        System.out.println("Budding physicist and cognitive scientist. Amateur saxophonist. Features Editor for Zelda Universe.");
+
+    //test
+    }
+
+}
+        
